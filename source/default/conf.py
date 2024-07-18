7d91476a2b9ada3d9f3f02eccb4fc24aa514a58e
Tsangyao Chen <tychen742@gmail.com>
2024-07-18 03:00:15 -0500

    committed @ 2024-0718-030015

--- source/default/conf.py
+++ source/default/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Introduction to Computer Science in C#'
 authors= u'Tsangyao (T.Y.) Chen'
-copyright = u''
+# copyright = u''
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
