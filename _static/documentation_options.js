9b597c39fb83b7ac9aff190d45ac5b2b64e653ad
tychen742 <tychen742@users.noreply.github.com>
2024-07-28 06:54:17 +0000

    deploy: fb637e646dda677eea9c655eca24b0d42eda9aef

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '27 Jul 2024',
+    VERSION: '28 Jul 2024',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
